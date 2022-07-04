0136755142c5085c04cd74376c65f9a1e7222a73
DESKTOP-8EGVUT9\user <aaaa@gmail.com>
2022-07-04 18:50:25 +0900

    5강 예제 - JSTL fn 라이브러리

--- src/main/java/com/example/lesson05/Lesson05Controller.java
+++ src/main/java/com/example/lesson05/Lesson05Controller.java
@@ -59,4 +59,10 @@ public class Lesson05Controller {
 		
 		return "lesson05/ex03";
 	}
+	
+	// http://localhost/lesson05/ex04
+	@RequestMapping("/lesson05/ex04")
+	public String ex04() {
+		return "lesson05/ex04";
+	}
 }
